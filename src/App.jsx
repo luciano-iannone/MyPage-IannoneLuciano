3334173a78ceb6700e2af2e09981fca22cff5f7c
Luciano Iannone <lucho.iannone.78@gmail.com>
2023-11-07 18:13:21 -0300

    2

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './style.css'
+import Categories from './components/categories/Categories'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProductList from './components/products/ProductList'
+import CategoriesProductList from './components/categories/CategoriesProductList'
+import Navbar from './components/Navbar/Navbar'
+import Cart from './components/Cart/Cart'
+import CartProvider from './context/CartContext'
+
+function App() {
+
+  return (
+    <>
+      <CartProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Categories />} />
+            <Route exact path="/products" element={<ProductList />} />
+            <Route exact path="/category/:categoryId" element={<CategoriesProductList />} />
+            <Route exact path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+
+    </>
+  )
+}
+
+export default App
